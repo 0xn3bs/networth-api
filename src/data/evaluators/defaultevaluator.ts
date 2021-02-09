c23066a832d1d83ad04f76b5ac03b366d95f6b67
Jonathan Bastnagel <j.bastnagel@gmail.com>
2021-02-08 23:29:52 -0600

    If a ticker gets multiple hits, choose the one with the largest MCap.

--- src/data/evaluators/defaultevaluator.ts
+++ src/data/evaluators/defaultevaluator.ts
@@ -6,7 +6,7 @@ const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 
 class DefaultEvaluator implements Evaluator {
-    async hydrateValuedAssets(valuedAssets: ValuedAsset[]): Promise<void> {
+	async hydrateValuedAssets(valuedAssets: ValuedAsset[]): Promise<void> {
         let tickers = new Set<string>();
 
         for(let i = 0; i < valuedAssets.length; ++i) {
@@ -40,17 +40,52 @@ class DefaultEvaluator implements Evaluator {
         }
     }
 
-    async tickerToCoinId(ticker: string, coins: any): Promise<any> {
+    //  TODO: Refactor so we make less calls to Coingecko
+    //        In theory, we should be able to include all the coins in one call
+    //        and do the de-dup logic last.
+    async tickerToCoin(ticker: string, coins: any): Promise<any> {
+        const potentials = new Array<any>();
+
         for(let i = 0; i < coins.length; ++i) {
             if(coins[i].symbol.toLowerCase() === ticker.toLowerCase()) {
-                return coins[i].id;
+                potentials.push(coins[i].id);
             }
+        }
+
+        const data = (await CoinGeckoClient.simple.price({
+                        ids: potentials,
+                        vs_currencies: ['usd'],
+                        include_last_updated_at: true,
+                        include_market_cap: true
+                    })).data;
+
+        const keys = Object.keys(data);
+
+        if(keys.length > 0){
+            //  If we get multiple hits, figure out which one has the largest market cap
+            //  that's probably the "right" one.
+
+            let largest_id = keys[0];
+            let largest = data[keys[0]];
+
+            for(let i = 0; i < keys.length; ++i) {
+                const c = data[keys[i]];
+
+                if(c.usd_market_cap > largest.usd_market_cap) {
+                    largest_id = keys[i];
+                    largest = c;
+                }
+            }
+
+            let r = {};
+            r[largest_id] = largest;
+            return r
         }
 
         return null;
     } 
 
-    async coinIdToTicker(id: string, coins: any) {
+    async coinToTicker(id: string, coins: any) {
         for(let i = 0; i < coins.length; ++i) {
             if(coins[i].id.toLowerCase() === id.toLowerCase()) {
                 return coins[i].symbol;
@@ -60,7 +95,7 @@ class DefaultEvaluator implements Evaluator {
 
     async getPrices(tickers: Set<string>): Promise<any> {
         let tickersA = Array.from(tickers);
-        let ids = Array<string>();
+        let ids = {};
 
         let prices = {};
 
@@ -68,24 +103,30 @@ class DefaultEvaluator implements Evaluator {
 
         for(let i = 0; i < tickersA.length; ++i) {
             const ticker = tickersA[i];
-            const id = await this.tickerToCoinId(ticker, coins);
+            const coin = await this.tickerToCoin(ticker, coins);
 
-            if(id != null) {
-                ids.push(id);
+            if(coin != null) {
+                const seconds = Math.floor(Date.now() / 1000);
+
+                var id = (Object.keys(coin))[0];
+
+                //  If it's over an hour old, consider it stale, don't include it
+                //  hopefully we'll get better pricing via 1inch.
+                var last_updated_at = coin[id].last_updated_at;
+                if(last_updated_at != null) {
+                    if((seconds - coin[id].last_updated_at) < 3600) {
+                        ids = {...ids, ...coin};
+                    }
+                }
             }
         }
 
-        const data = (await CoinGeckoClient.simple.price({
-                        ids: ids,
-                        vs_currencies: ['usd'],
-                    })).data;
-
-        var dataKeys = Object.keys(data);
+        var dataKeys = Object.keys(ids);
 
         for(var i = 0; i < dataKeys.length; ++i) {
             const key = dataKeys[i];
-            const ticker = (await this.coinIdToTicker(dataKeys[i], coins)).toUpperCase();
-            const price = data[key]['usd'];
+            const ticker = (await this.coinToTicker(dataKeys[i], coins)).toUpperCase();
+            const price = ids[key]['usd'];
             prices[ticker] = price;
         }
 
